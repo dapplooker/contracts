d7b8cae349e4f392a65fb42e56b288e3bfccabcf
Vikash Choubey <choubey@Vikashs-MacBook-Pro.local>
2023-02-10 12:13:29 +0530

    Formatting

--- contracts/PaymentVault.sol
+++ contracts/PaymentVault.sol
@@ -16,7 +16,15 @@ contract PaymentVault is Initializable {
 		owner = payable(msg.sender);
 	}
 
-	function depositFunds(IERC20Upgradeable tokenToLock, uint256 amount) public {
+	/**
+ 	 * @notice Deposit `amount` of `erc20` tokens from this user's wallet to `contract`.
+     * @param tokenToLock ERC20 token to be deposited
+     * @param amount of tokens to deposit
+     */
+	function depositFunds(
+		IERC20Upgradeable tokenToLock,
+		uint256 amount
+	) public {
 		uint256 tokenBalanceSender = tokenToLock.balanceOf(address(msg.sender));
 		require(tokenBalanceSender >= amount, "Balance for wallet is low!!!");
 		tokenToLock.safeTransferFrom(msg.sender, address(this), amount);
@@ -24,24 +32,44 @@ contract PaymentVault is Initializable {
 		emit depositFundCompleted(msg.sender, amount);
 	}
 
-	function returnBalance(IERC20Upgradeable token, address walletAddress) public view returns (uint256) {
+	/**
+  	 * @notice Return balance of `erc20` token of user's wallet.
+     * @param tokenToLock ERC20 token's balance to be checked
+     * @param walletAddress for which balance needs to be checked
+     */
+	function returnBalance(
+		IERC20Upgradeable token,
+		address walletAddress
+	) public view returns (uint256) {
 		uint256 tokenAmount = token.balanceOf(address(walletAddress));
 		return tokenAmount;
 	}
 
-	function tokenBalance(IERC20Upgradeable token) public view returns (uint256) {
+	/**
+     * @notice Return balance of `erc20` token of contract wallet.
+     * @param tokenToLock ERC20 token's balance to be checked
+     */
+	function tokenBalance(
+		IERC20Upgradeable token
+	) public view returns (uint256) {
 		uint256 tokenTotalBalance = token.balanceOf(address(this));
 		return tokenTotalBalance;
 	}
 
-	function withdrawFunds(IERC20Upgradeable tokenToLock, uint256 amount) public {
+	/**
+     * @notice Withdraw `amount` of `erc20` tokens from `contract` to owner's wallet.
+     * @param tokenToLock ERC20 token to be withdrawn
+     * @param amount of tokens to withdrawn
+     */
+	function withdrawFunds(
+		IERC20Upgradeable tokenToWithdrawn,
+		uint256 amount
+	) public {
 		require(msg.sender == owner, "Only owner can withdraw funds");
 		require(amount > 0, "Withdraw amount must be greater than 0");
-		require(tokenToLock.balanceOf(address(this)) >= amount, "Insufficient funds");
+		require(tokenToWithdrawn.balanceOf(address(this)) >= amount, "Insufficient funds");
+		tokenToWithdrawn.safeTransfer(msg.sender, amount);
 
-		tokenToLock.safeTransfer(msg.sender, amount);
-
-		emit withdrawFundsCompleted(address(tokenToLock), msg.sender, amount);
-		// owner.transfer(amount);
+		emit withdrawFundsCompleted(address(tokenToWithdrawn), msg.sender, amount);
 	}
 }
