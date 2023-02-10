30e91061eaaec48f4b9d6d222f69ff9da7fc1335
Vikash Choubey <choubey@Vikashs-MacBook-Pro.local>
2023-02-10 11:45:46 +0530

    Removed artifacts folder

--- scripts/config/PaymentABI.ts
+++ scripts/config/PaymentABI.ts
@@ -1,1 +1,1 @@
-export const contractABI = require("../../artifacts/contracts/MultiPaymentSupport.sol/MultiTokenPaymentGateway.json");
+export const contractABI = require("../../artifacts/contracts/PaymentVault.sol/PaymentVault.json");
--- scripts/upgrade-deploy.ts
+++ scripts/upgrade-deploy.ts
@@ -2,7 +2,7 @@ const upgradeHardhatObj = require("hardhat")
 
 async function deployUpgradeContract() {
     const contractFactory = await upgradeHardhatObj.ethers.getContractFactory("PaymentVault");
-    const contractUpgrade = await upgradeHardhatObj.upgrades.upgradeProxy("0x6fE11011840a0Dc12F901f26b93cd6d2b374f67D", contractFactory);
+    const contractUpgrade = await upgradeHardhatObj.upgrades.upgradeProxy("0xC8ADf3859ca4eDC5726e16962e51E67321888EB8", contractFactory);
     console.log(`Contract upgraded deployed to ${contractUpgrade.address}`);
 }
 
