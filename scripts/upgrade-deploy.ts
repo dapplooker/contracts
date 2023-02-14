f9bc2854a1b12033ae2be09ea5ae2bb6537573ce
Vikash Choubey <choubey@Vikashs-MacBook-Pro.local>
2023-02-14 17:52:58 +0530

    Upgrade proxy script update

--- scripts/upgrade-deploy.ts
+++ scripts/upgrade-deploy.ts
@@ -4,7 +4,7 @@ const upgradeHardhatObj = require("hardhat")
 
 async function deployUpgradeContract() {
     const contractFactory = await upgradeHardhatObj.ethers.getContractFactory("PaymentVault");
-    const contractUpgrade = await upgradeHardhatObj.upgrades.upgradeProxy("0xd2736449c79b33151d633a77feEDc8555EBE1169", contractFactory);
+    const contractUpgrade = await upgradeHardhatObj.upgrades.upgradeProxy(process.env.PROXY_CONTRACT_ADDRESS, contractFactory);
     console.log(`Contract upgraded deployed to ${contractUpgrade.address}`);
 }
 
