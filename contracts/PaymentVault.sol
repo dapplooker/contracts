e774f9b72bb9bbfefa166eb365f028493f109176
Vikash Choubey <choubey@Vikashs-MacBook-Pro.local>
2023-02-15 14:45:56 +0530

    Review comments

--- contracts/PaymentVault.sol
+++ contracts/PaymentVault.sol
@@ -21,42 +21,44 @@ contract PaymentVault is Initializable {
 
 	/** Events which will be emitted on operation completes. */
 	event DepositCompleted(address sender, uint256 amount);
-	event withdrawCompleted(address tokenWithdraw, address sentTo, uint256 amount);
+	event WithdrawCompleted(address token, address beneficiary, uint256 amount);
 
 	function initialize() public initializer {
 		owner = msg.sender;
 	}
 
 	/**
- 	 * @notice Deposit `amount` of `erc20` tokens from this user's wallet to `contract`.
-     * @param token ERC20 token to be deposited
-     * @param amount of tokens to deposit
+ 	 * @dev Deposit `amount` of `erc20` tokens from this user's wallet to `contract`.
+ 	 *
+     * @param _token ERC20 token to be deposited.
+     * @param _amount of tokens to deposit.
      */
 	function deposit(
-		IERC20Upgradeable token,
-		uint256 amount
+		IERC20Upgradeable _token,
+		uint256 _amount
 	) public {
-		uint256 tokenBalanceSender = token.balanceOf(address(msg.sender));
-		require(tokenBalanceSender >= amount, "Your wallet balance for token is low");
-		token.safeTransferFrom(msg.sender, address(this), amount);
+		uint256 balance = _token.balanceOf(address(msg.sender));
+		require(balance >= _amount, "Your wallet balance for token is low");
+		_token.safeTransferFrom(msg.sender, address(this), _amount);
 
-		emit DepositCompleted(msg.sender, amount);
+		emit DepositCompleted(msg.sender, _amount);
 	}
 
 	/**
-     * @notice Withdraw `amount` of `erc20` tokens from `contract` to owner's wallet.
-     * @param token ERC20 token to be withdrawn
-     * @param amount of tokens to withdrawn
+     * @dev Withdraw `amount` of `erc20` tokens from `contract` to owner's wallet.
+     *
+     * @param _token ERC20 token to be withdrawn.
+     * @param _amount of tokens to withdrawn.
      */
 	function withdraw(
-		IERC20Upgradeable token,
-		uint256 amount
+		IERC20Upgradeable _token,
+		uint256 _amount
 	) public {
 		require(msg.sender == owner, "Only owner can withdraw funds");
-		require(amount > 0, "Withdraw amount must be greater than 0");
-		require(token.balanceOf(address(this)) >= amount, "Insufficient funds");
-		token.safeTransfer(msg.sender, amount);
+		require(_amount > 0, "Withdraw amount must be greater than 0");
+		require(_token.balanceOf(address(this)) >= _amount, "Insufficient funds");
+		_token.safeTransfer(msg.sender, _amount);
 
-		emit withdrawCompleted(address(token), msg.sender, amount);
+		emit WithdrawCompleted(address(_token), msg.sender, _amount);
 	}
 }
